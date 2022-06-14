9d27ba79236c0bac1e658266970ab0c517c55b55
Tara Lehman <tara.lehman@asurion.com>
2022-06-14 14:48:58 -0500

    simple counter example not using hooks

---
+++ example.js
@@ -0,0 +1,26 @@
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    document.title = `Count: ${this.state.count}`;
+  }
+
+  componentDidUpdate() {
+    document.title = `Count: ${this.state.count}`;
+  }
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
